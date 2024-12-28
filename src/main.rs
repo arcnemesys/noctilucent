02aaddcae31ab8b6be4bfa18bb3363c397c757ff
arcnemesys <accelerenzo@proton.me>
2024-12-27 20:23:35 -0500

    Added simple proxy functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use actix_web::{ HttpRequest, HttpResponse };
+use actix_web::{ web::{self, Data}, HttpRequest, HttpResponse };
 
+
+struct AppState {
+    servers: Vec<String>
+}
 struct LoadBalancer {
     port: u16,
     servers: Vec<String>
@@ -15,8 +19,12 @@ impl LoadBalancer {
     }
 
     pub async fn run(&self) {
+        let data = web::Data::new(AppState {
+            servers: self.servers.clone()
+        });
         HttpServer::new(move || App::new()
-            .default_service(web::to(handler)))
+            .default_service(web::to(Self::handler))
+            .app_data(data.clone()))
         .bind(("127.0.0.1", self.port))
         .unwrap()
         .run()
@@ -24,10 +32,38 @@ impl LoadBalancer {
         .unwrap();
     }
 
-    async fn handler(req: HttpRequest) -> HttpResponse {
+    async fn handler(
+        req: HttpRequest,
+        // Extractor for shared app state
+        data: web::Data<AppState>,
+        // Extractor to grab request payload as raw bytes 
+        bytes: web::Bytes) -> Result<HttpResponse, Error> {
+    
+        // Grab the address to forward the request to 
+        let server = data.servers[0].clone();
+        let uri = format!("{}{}", server, req.uri());
+
+        let request_builder = client
+            .request(req.method().clone(), uri)
+            .headers(req.headers().into())
+            .body(bytes);
+       
+        // Actual request forwarding
+        let response = request_builder.send().await?;
+        
+        // Get response and send it back to client
+        let mut response_builder = HttpResponse::build(response.status());
+        
+        for header in response.headers().iter() {
+            response_builder.append_header(h);
+        }
+
+        let body = response.bytes().await?;
+        Ok(response_builder.body(body))
         HttpResponse::Ok().body("hello!")
     }
 }
 fn main() {
     println!("Hello, world!");
 }
+
